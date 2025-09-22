96dec3cfb08e83f507a413e0679408e5558ca3db
Dimitrije Zoroski <dimitrije.zoroski@gowi.rs>
2025-09-22 14:27:24 +0200

    Seed terms, add start page

--- Glossary.Infrastructure/Data/AppDbContext.cs
+++ Glossary.Infrastructure/Data/AppDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Glossary.Domain.Entities;
+﻿using Glossary.Domain;
+using Glossary.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -17,6 +18,38 @@ namespace Glossary.Infrastructure.Data
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.ApplyConfiguration(new TermConfiguration());
+
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Term>().HasData(
+                new Term
+                {
+                    Id = Guid.Parse("11111111-1111-1111-1111-111111111111"),
+                    Name = "abyssal plain",
+                    Definition = "The ocean floor offshore from the continental margin, usually very flat with a slight slope.",
+                    Status = Status.Published,
+                    AuthorId = Guid.Parse("11111111-1111-1111-1111-111111111111"),
+                    CreatedAt = DateTime.UtcNow
+                },
+                new Term
+                {
+                    Id = Guid.Parse("22222222-2222-2222-2222-222222222222"),
+                    Name = "accrete",
+                    Definition = "v. To add terranes (small land masses or pieces of crust) to another, usually larger, land mass.",
+                    Status = Status.Published,
+                    AuthorId = Guid.Parse("11111111-1111-1111-1111-111111111111"),
+                    CreatedAt = DateTime.UtcNow
+                },
+                new Term
+                {
+                    Id = Guid.Parse("33333333-3333-3333-3333-333333333333"),
+                    Name = "alkaline",
+                    Definition = "Term pertaining to a highly basic, as opposed to acidic, substance. For example, hydroxide or carbonate of sodium or potassium.",
+                    Status = Status.Draft,
+                    AuthorId = Guid.Parse("11111111-1111-1111-1111-111111111111"),
+                    CreatedAt = DateTime.UtcNow
+                }
+            );
         }
     }
 }
---
+++ Glossary.Infrastructure/Migrations/20250922121818_SeedTerms.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Glossary.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedTerms : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Terms",
+                columns: new[] { "Id", "AuthorId", "CreatedAt", "Definition", "Name", "Status" },
+                values: new object[,]
+                {
+                    { new Guid("11111111-1111-1111-1111-111111111111"), new Guid("11111111-1111-1111-1111-111111111111"), new DateTime(2025, 9, 22, 12, 18, 17, 306, DateTimeKind.Utc).AddTicks(3187), "The ocean floor offshore from the continental margin, usually very flat with a slight slope.", "abyssal plain", 1 },
+                    { new Guid("22222222-2222-2222-2222-222222222222"), new Guid("11111111-1111-1111-1111-111111111111"), new DateTime(2025, 9, 22, 12, 18, 17, 306, DateTimeKind.Utc).AddTicks(3191), "v. To add terranes (small land masses or pieces of crust) to another, usually larger, land mass.", "accrete", 1 },
+                    { new Guid("33333333-3333-3333-3333-333333333333"), new Guid("11111111-1111-1111-1111-111111111111"), new DateTime(2025, 9, 22, 12, 18, 17, 306, DateTimeKind.Utc).AddTicks(3194), "Term pertaining to a highly basic, as opposed to acidic, substance. For example, hydroxide or carbonate of sodium or potassium.", "alkaline", 0 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Terms",
+                keyColumn: "Id",
+                keyValue: new Guid("11111111-1111-1111-1111-111111111111"));
+
+            migrationBuilder.DeleteData(
+                table: "Terms",
+                keyColumn: "Id",
+                keyValue: new Guid("22222222-2222-2222-2222-222222222222"));
+
+            migrationBuilder.DeleteData(
+                table: "Terms",
+                keyColumn: "Id",
+                keyValue: new Guid("33333333-3333-3333-3333-333333333333"));
+        }
+    }
+}
